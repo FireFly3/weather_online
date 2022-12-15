45a4da2ed42534d6a3785a8d5042e43b8c9f97bd
FireFly3 <4irelly@gmail.com>
2022-12-15 12:00:53 +0100

    first component

---
+++ src/components/Info.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Info extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Weather online</h2>
+                <p>Check the weather in your city</p>
+            </div>
+        );
+    }
+}
+
+export {Info};
